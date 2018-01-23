6e934abb6b1fa7c1afa8cf8d63c63e5ee5675444
Peter Hofmann <4168@htl.rennweg.at>
2018-01-23 09:20:54 +0100

    erster Punkt fertig

--- index.php
+++ index.php
@@ -6,4 +6,18 @@
  * Time: 08:40
  */
 
+namespace hofma\ajaxue;
+require_once __DIR__.'/vendor/autoload.php';
 
+use \HTL3R\Flags\Flags as FlagTypes;
+use \HTL3R\Flags\Interfaces as Interfaces;
+
+$flag[] = new FlagTypes\RectangleFlag("Coole Menschen Land", 300, 50, 20, "#53f442");
+$flag[] = new FlagTypes\RectangleFlag("blöde Menschen Land", 300, 50, 20, "#f4c842");
+$flag[] = new FlagTypes\RectangleFlag("normale Menschen Land", 300, 50, 20, "#4162f4");
+
+
+foreach ($flag as $singleFlag){
+    /* @var $flag interfaces\FlagInterface*/
+    echo $singleFlag."<br/>".$singleFlag->calculateArea()."<br/>"."<br/>";
+}
